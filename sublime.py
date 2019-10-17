0d5da9608f19732ae74a13f7254c97e71054e385
zildj <zildj100@gmail.com>
2019-10-17 11:20:46 -0400

    add gitignore for data folder pull more lime-specific data pretty-up console output

--- sublime.py
+++ sublime.py
@@ -7,16 +7,16 @@ import os.path
 def getLimes(lat, lon, token):
     URL = f"https://api.multicycles.org/v1?access_token={token}"
     data = {"query": "query {lime (lat:"+lat+", lng:"+lon +
-            ") {id, type, attributes, lat, lng, provider {name}}}"}
+            ") {id, type, attributes, lat, lng, provider {name}, limeFields {status, plate_number, last_activity_at, type_name, battery_level, meter_range}}}"}
     r = requests.post(url=URL, data=data)
     output = {}
     if r.status_code is 200:
         data = r.json()['data']['lime']
         for lime in data:
             output[lime['id']] = lime
-            print(lime['id'])
     else:
         print(f"<{r.status_code}> {r.text}")
+    print(f"Found {len(output)} scooters at ({lat}, {lon})")
     return output
 
 
@@ -26,12 +26,11 @@ with open('settings.json') as json_file:
     coords = jsonData['coords']
     token = jsonData['token']
     for coord in coords:
-        print(f"lat: {coord['lat']}, lon: {coord['lon']}")
         tempData = getLimes(coord['lat'], coord['lon'], token)
         for key in tempData:
             if (key not in limeData.keys()):
                 limeData[key] = tempData[key]
-print(limeData)
+print(f"Found {len(limeData)} scooters across {len(coords)} locations")
 path = os.path.join(os.path.dirname(
     __file__), f'data/{datetime.today().strftime("%Y-%m-%d %H-%M-%S")}.txt')
 with open(path, 'w') as outfile:
